451573e608eb94d2ce18849a0bc406b440acf09d
aalok-sathe <asathe@mit.edu>
2021-09-15 14:37:27 -0400

    reduce timeout to 15s (from 10+ minutes); preload app

--- singularity-run-container.sh
+++ singularity-run-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-singularity exec docker://aloxatel/berkeleyparser /bin/bash -c "JAVA_HOME=/usr/local/openjdk-18 gunicorn interactive:app -t 1000"
+singularity exec docker://aloxatel/berkeleyparser /bin/bash -c "JAVA_HOME=/usr/local/openjdk-18 gunicorn interactive:app -t 15 --preload --config gunicorn_config.py" 
 # singularity run --no-home docker://aloxatel/berkeleyparser:latest
